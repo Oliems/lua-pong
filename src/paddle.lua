64bfce7659dd46075b68073d015e5553b67007ef
Oliems <100418841+Oliems@users.noreply.github.com>
2022-10-27 18:10:04 +0200

    Changed keys to control the paddle.

--- src/paddle.lua
+++ src/paddle.lua
@@ -4,18 +4,18 @@ function move_paddles()
 			player1.y = player1.y - paddle.dy
 		end
 	end
-	if love.keyboard.isDown("w") then
+	if love.keyboard.isDown("s") then
 		if player1.y < window.h - paddle.h - paddle.dy then
 			player1.y = player1.y + paddle.dy
 		end
 	end
 
-	if love.keyboard.isDown("o") then
+	if love.keyboard.isDown("p") then
 		if player2.y > paddle.dy then
 			player2.y = player2.y - paddle.dy
 		end
 	end
-	if love.keyboard.isDown("p") then
+	if love.keyboard.isDown("l") then
 		if player2.y < window.h - paddle.h - paddle.dy then
 			player2.y = player2.y + paddle.dy
 		end
